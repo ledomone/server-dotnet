6894f453bcfdb7a166fc666dc5a4c06999707def
Ledom One <ledomone@outlook.com>
2025-06-19 17:56:54 +0200

    Prepare for deployment using docker

---
+++ server-dotnet.migrator/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using server_dotnet.Infrastructure.Data;
+
+public class Migrator
+{
+    public static void Main(string[] args)
+    {
+        var builder = Host.CreateDefaultBuilder(args);
+        builder.ConfigureServices((hostContext, services) =>
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(hostContext.Configuration.GetConnectionString("DefaultConnection")));
+        });
+
+        using var host = builder.Build();
+        using var scope = host.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        Console.WriteLine("Applying database migrations...");
+        context.Database.Migrate();
+        Console.WriteLine("Migrations applied successfully.");
+    }
+}
